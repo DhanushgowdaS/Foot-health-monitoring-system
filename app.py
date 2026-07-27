6259cb924105268cfead73cf58ca6eb72d5cc42a
Dhanush S <dhanushgowdas222@gmail.com>
2026-07-27 21:37:11 +0530

    Update app.py

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ import streamlit as st
 import pandas as pd
 import requests
 
-API_URL = "https://smart-footwear-api.onrender.com"
+API_URL = "https://ai-footwear-ulcer-detection.onrender.com"
 
 st.set_page_config(
     page_title="Smart Footwear Dashboard",
